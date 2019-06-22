a001f12dd6ea9750f53c7049ce1eaf14df18e733
Alvian Teddy Cahya Putra <alvianteddy@gmail.com>
2019-06-22 10:10:00 +0700

    Mempelajari nested loop

---
+++ src/perulangan/BelajarNestedLoop.java
@@ -0,0 +1,20 @@
+package perulangan;
+
+public class BelajarNestedLoop {
+    public static void main(String[] args) {
+
+        int ulang = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    ulang += 1;
+                    System.out.println();
+                    System.out.println("Perulangan ke- " + ulang);
+                    System.out.println("i: " + i + "     j: " + j + "      k: " + k);
+                }
+            }
+        }
+    }
+}
+
