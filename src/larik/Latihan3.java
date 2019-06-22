f2a555cdc48fc8431c060301e674b34a4f88e5f6
Alvian Teddy Cahya Putra <alvianteddy@gmail.com>
2019-06-22 07:47:04 +0700

    Membuat file Latihan 3

---
+++ src/larik/Latihan3.java
@@ -0,0 +1,73 @@
+package larik;
+
+import java.util.Scanner;
+
+public class Latihan3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int matriksPertama[][] = new int[2][3];
+        int matriksKedua[][] = new int[2][3];
+        int hasil[][] = new int[2][3];
+
+        //      Mengisi elemen matriks pertama
+        System.out.println("Masukkan nilai matriks 1: ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Matriks[" + i + "][" + j + "]: ");
+                matriksPertama[i][j] = input.nextInt();
+            }
+        }
+
+        //      Mengisi elemen matriks kedua
+        System.out.println();
+        System.out.println("Masukkan nilai matriks 2: ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Matriks[" + i + "][" + j + "]: ");
+                matriksKedua[i][j] = input.nextInt();
+            }
+        }
+
+        //      Menampilkan atau mencetak elemen matriks kedua ke layar console
+        System.out.println();
+        System.out.println("Matiks Pertama");
+        for (int i = 0; i < 2; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" " + matriksPertama[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+
+        //      Menampilkan atau mencetak elemen matriks kedua ke layar console
+        System.out.println();
+        System.out.println("Matiks Kedua");
+        for (int i = 0; i < 2; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" " + matriksKedua[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+
+//      mengurangkan kedua matriks
+        System.out.println();
+        System.out.println("Hasil penjumlahan Matriks");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                hasil[i][j] = matriksPertama[i][j] - matriksKedua[i][j];
+            }
+        }
+//      menampilkan hasil pengurangan matriks
+        System.out.println();
+        for (int i = 0; i < 2; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" " + hasil[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+    }
+}
