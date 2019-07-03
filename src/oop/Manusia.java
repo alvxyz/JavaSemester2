dd8d3b5b4cf761a16a1bf0bd39c9efaa17a58b45
Alvian Teddy Cahya Putra <alvianteddy@gmail.com>
2019-07-03 08:06:37 +0700

    belajar OOP kelas

---
+++ src/oop/Manusia.java
@@ -0,0 +1,44 @@
+package oop;
+
+public class Manusia {
+
+    // variabel
+
+    String nama;
+    int umur;
+    int beratBadan;
+    int tinggiBadan;
+
+
+    // default konstraktor
+    public Manusia() {
+
+    }
+
+    public Manusia(String nama, int umur, int beratBadan, int tinggiBadan) {
+        this.nama = nama;
+        this.umur = umur;
+        this.beratBadan = beratBadan;
+        this.tinggiBadan = tinggiBadan;
+    }
+
+    // method
+    void berjalan() {
+
+    }
+
+    void berlari() {
+    }
+
+    void makan() {
+    }
+
+    void minum() {
+    }
+
+    // method main
+    public static void main(String[] args) {
+        // tidak akan tampil apa-apa dikarenakan tidak ada statment
+    }
+
+}
--- src/perulangan/BelajarNestedLoop.java
+++ src/perulangan/BelajarNestedLoop.java
@@ -5,13 +5,14 @@ public class BelajarNestedLoop {
 
         int ulang = 0;
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-//                for (int k = 0; k < 2; k++) {
-                ulang += 1;
-                System.out.println();
-                System.out.println("Perulangan ke- " + ulang);
-                System.out.println("i: " + i + "     j: " + j);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    ulang += 1;
+                    System.out.println();
+                    System.out.println("Perulangan ke- " + ulang);
+                    System.out.println("i: " + i + "     j: " + j + "        k: " + k);
+                }
             }
         }
     }
